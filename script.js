df2f3b00d63c23a5f233d367c77abf1913a4ce38
Leonardo Amaro da Silva <93151770+leonardo-amaro@users.noreply.github.com>
2024-08-24 21:57:27 -0300

    Adicionado função que remove tarefas.

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function adicionaTarefa() {
   let tarefa = document.querySelector("#descricao");
   let lista = document.querySelector("#lista");
   let li = document.createElement("li");
-  
   li.innerHTML = `${tarefa.value}`;
   lista.appendChild(li);
+  tarefa.value = "";
 };
+
+function removeTarefa(evento) {
+  try {
+    let lista = document.querySelector("#lista");
+    lista.removeChild(evento.target);
+  } catch(error) {
+    console.error("Operação inválida");
+  }
+};
+
+document.addEventListener("dblclick", removeTarefa);
