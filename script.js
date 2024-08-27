60008bc0592c2f7e8a7946b442b8de624afdb244
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2024-08-27 09:54:35 -0300

    validação na função de adcionar tarefas.

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function adicionaTarefa() {
   let tarefa = document.querySelector("#descricao");
-  let lista = document.querySelector("#lista");
-  let li = document.createElement("li");
-  li.innerHTML = `${tarefa.value}`;
-  lista.appendChild(li);
-  tarefa.value = "";
+  if(tarefa.value == "") {
+    window.alert("ATENÇÃO: Tarefas precisam ter uma descrição!");
+  } else {
+    let lista = document.querySelector("#lista");
+    let li = document.createElement("li");
+    li.innerHTML = `${tarefa.value}`;
+    lista.appendChild(li);
+    tarefa.value = "";
+  }
 };
 
 function removeTarefa(evento) {
