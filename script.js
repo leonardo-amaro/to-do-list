3b2b8a89abdf211ceb7b72ba9a6280cbade109fe
Leonardo Amaro <leonardo.amarosilva01@gmail.com>
2024-08-30 11:22:22 -0300

    Adicionada função que armazena tarefas no localStorage

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ const lista = document.querySelector("#lista");
 const descricao = document.querySelector("#descricao");
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
 
+function armazenaTarefa(tarefa) {
+  const tarefaAtual = {
+    "tarefa": tarefa
+  };
+  tarefas.push(tarefaAtual);
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+};
+
 function adicionaTarefa() {
   if(descricao.value.trim() == "") {
     window.alert("ATENÇÃO: Tarefas precisam ter uma descrição!");
